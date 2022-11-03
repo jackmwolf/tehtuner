86a871ec3fb719d76137b8aac6d9ffdfa23c67e2
Jack M Wolf <38739875+jackmwolf@users.noreply.github.com>
2022-11-03 14:02:16 -0500

    Reversion and add test for variable name issues

---
+++ tests/testthat/test-main.R
@@ -0,0 +1,15 @@
+
+test_that(
+  "Identifies Trt and Y", {
+    dat <- tehtuner_example[c(1:10, 101:110), ]
+    names(dat)[names(dat) == "Trt"] <- "A"
+    names(dat)[names(dat) == "Y"] <- "Outcome"
+
+    expect_no_error(
+      tunevt(
+        data = dat, Y = "Outcome", Trt = "A",
+        step1 = "mars", step2 = "rtree", alpha0 = 0.95, p_reps = 1
+        )
+    )
+  }
+)
